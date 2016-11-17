53566b8c094a76c0dabfc6879b55ba917d8188b8
Murriouz <Murriouz@gmail.com>
2016-11-17 11:54:34 +0800

    only support one language

--- index.js
+++ index.js
@@ -10,25 +10,24 @@ var SECOND = 1000
     , extend = require("xtend")
 
 srt.fromString = fromString
-srt.merge = merge
 
 module.exports = srt
 
-function srt(language, fileName, callback) {
-    fs.readFile(fileName, partial(returnParsedData, language, callback))
+function srt(fileName, callback) {
+    fs.readFile(fileName, partial(returnParsedData, callback))
 }
 
-function returnParsedData(language, callback, err, data) {
+function returnParsedData(callback, err, data) {
     if (err) {
         return callback(err)
     }
 
-    callback(null, fromString(language, data.toString()))
+    callback(null, fromString(data.toString()))
 }
 
-function fromString(language, stringData) {
+function fromString(stringData) {
     var segments = stringData.split((stringData.search("\n\r\n") != -1) ? "\n\r\n" : "\n\n" )
-    return reduce(segments, createSrtData, language, [])
+    return reduce(segments, createSrtData, [])
 }
 
 function createSrtData(memo, string) {
@@ -43,15 +42,12 @@ function createSrtData(memo, string) {
         , startTime = parseTime(times[0])
         , endTime = parseTime(times[1])
         , text = lines.slice(2).join("\n")
-        , languages = {}
-
-    languages[this] = text
 
     memo.push({
         number: number
         , startTime: startTime
         , endTime: endTime
-        , languages: languages
+        , text: text
     })
 
     return memo
@@ -69,27 +65,4 @@ function parseTime(timeString) {
         MINUTE * minutes +
         SECOND * seconds +
         milliSeconds
-}
-
-function merge(srt) {
-    var srts = slice.call(arguments)
-
-    return reduce(srts, mixTogether)
-}
-
-function mixTogether(srtOne, srtTwo) {
-    return map(srtOne, insertOtherSrtLanguage, srtTwo)
-}
-
-function insertOtherSrtLanguage(caption, index) {
-    var otherSrt = this
-        , otherCaptions = this[index]
-        , languages = extend({}, caption.languages, otherCaptions.languages)
-
-    return {
-        startTime: caption.startTime
-        , endTime: caption.endTime
-        , number: caption.number
-        , languages: languages
-    }
 }
